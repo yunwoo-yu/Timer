16d454d430de223c12e5e701311c4551a0021099
Ycow <skypnal12@gmail.com>
2022-11-08 15:47:10 +0900

    add:시험시간 지남 구현

--- index.js
+++ index.js
@@ -4,28 +4,99 @@ const secBtn = document.querySelector("#sec");
 const startBtn = document.querySelector(".start-btn");
 const resetBtn = document.querySelector(".reset-btn");
 
-let timer = 0;
+let time = 0;
+let timer;
 let timeHour = 0;
 let timeMin = 0;
 let timeSec = 0;
 
-secBtn.textContent = timeSec;
+hrsBtn.textContent = timeSec;
+minBtn.textContent = timeMin;
+secBtn.textContent = timeHour;
 
 const timerStart = () => {
-  setInterval(() => {
-    if (timeSec === 0) {
-      clearInterval(timerStart);
+  timer = setInterval(() => {
+    time--;
+    timeSec--;
+
+    if (timeSec < 10) {
+      secBtn.textContent = timeSec.toString().padStart(2, "0");
+    } else {
+      secBtn.textContent = timeSec;
     }
-    timeSec -= 1;
-    secBtn.textContent = timeSec;
+    if (timeMin >= 1 && timeSec === 0) {
+      timeMin--;
+      minBtn.textContent = timeMin;
+      timeSec = 60;
+      secBtn.textContent = timeSec;
+    }
+
+    if (timeSec < 1 && timeMin >= 1) {
+      timeMin--;
+      minBtn.textContent = timeMin;
+      timeSec = 59;
+      secBtn.textContent = timeSec;
+    }
+    console.log(timeSec);
+    if (timeSec < 1 && timeMin < 1 && timeHour >= 1) {
+      timeHour--;
+      hrsBtn.textContent = timeHour;
+      timeMin = 59;
+      minBtn.textContent = timeMin;
+      timeSec = 59;
+      secBtn.textContent = timeSec;
+    }
+
+    if (timeSec < 1 && timeMin < 1 && timeHour < 1) {
+      clearInterval(timer);
+    }
   }, 1000);
 };
 
+const timerReset = () => {
+  clearInterval(timer);
+  time = 0;
+  timeHour = 0;
+  timeMin = 0;
+  timeSec = 0;
+  hrsBtn.textContent = timeSec;
+  minBtn.textContent = timeMin;
+  secBtn.textContent = timeHour;
+};
+
 secBtn.addEventListener("click", () => {
-  timeSec += 10;
+  time += 10;
+  timeSec = time % 60;
   secBtn.textContent = timeSec;
+  startBtn.classList.add("active");
+  resetBtn.classList.add("active");
+});
+
+minBtn.addEventListener("click", () => {
+  timeMin++;
+  if (timeMin > 60) {
+    timeMin = 60;
+  } else {
+    time += 60;
+  }
+  console.log(time);
+  minBtn.textContent = timeMin;
+  startBtn.classList.add("active");
+  resetBtn.classList.add("active");
+});
+
+hrsBtn.addEventListener("click", () => {
+  time += 3600;
+  timeHour = Math.floor(time / 3600);
+  hrsBtn.textContent = timeHour;
+  startBtn.classList.add("active");
+  resetBtn.classList.add("active");
 });
 
 startBtn.addEventListener("click", () => {
   timerStart();
+});
+
+resetBtn.addEventListener("click", () => {
+  timerReset();
 });
